d22832b5b3841509bbd5cc11d0763c4441d3cd9b
LeeYongHoon <39607133+YongHoonLeee@users.noreply.github.com>
2019-06-03 14:21:10 +0900

    FCF.cs  주석 추가.
    각 메소드 설명 추가

--- Capstones/Capstone_test_1/Capstone_test_1/FCF.cs
+++ Capstones/Capstone_test_1/Capstone_test_1/FCF.cs
@@ -15,9 +15,9 @@ namespace Capstone_test_1
     {
         public FCF()
         {
-            InitializeComponent();
-            comboBox1.Items.Add("*.txt");
-            comboBox1.Items.Add("*.jpg");
+            InitializeComponent(); 
+            comboBox1.Items.Add("*.txt"); // 확장자 필터링 부분 원하는 필터 추가하는 부분 
+            comboBox1.Items.Add("*.jpg"); // 추후에 확장자도 입력받아서 추가하는 기능 추가할 예정
             comboBox1.Items.Add("*.mp3");
             comboBox1.Items.Add("*.mp4");
             comboBox1.Items.Add("*.*");
@@ -26,12 +26,12 @@ namespace Capstone_test_1
             comboBox1.Items.Add("*.zip");
         }
 
-        private void Open_file_click(object sender, EventArgs e)
+        private void Open_file_click(object sender, EventArgs e) 
         {
             ChooseFolder();
         }
         
-        public void ChooseFolder()
+        public void ChooseFolder() // 탐색 할 폴더 경로 입력받는 함수
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             if (fbd.ShowDialog() == DialogResult.OK)
@@ -39,7 +39,7 @@ namespace Capstone_test_1
                 textBox1.Text = fbd.SelectedPath;
             }
         }
-        public void ChooseFolder2()
+        public void ChooseFolder2() // 복사 할 경로 입력받는 함수
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             if (fbd.ShowDialog() == DialogResult.OK)
@@ -63,39 +63,39 @@ namespace Capstone_test_1
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //  Filtering 부분
         {
-            String extension = comboBox1.SelectedItem.ToString();
+            String extension = comboBox1.SelectedItem.ToString(); //확장자 입력받기
             String srcDir = textBox1.Text;
-            String DestDir = textBox3.Text;
-            DateTime st = starttimepicker.Value;
+            String DestDir = textBox3.Text; // 경로 지정된거 탐색
+            DateTime st = starttimepicker.Value; //시간필터링 부분
             DateTime ed = endtimepicker.Value;
             DirectoryInfo dir = new DirectoryInfo($"{srcDir}");
-            FileInfo[] files = dir.GetFiles($"{extension}", SearchOption.AllDirectories);
-
-            Regex regex = new Regex(textBox4.Text);
+            FileInfo[] files = dir.GetFiles($"{extension}", SearchOption.AllDirectories); // SearchOption.AllDirectories
+                                                                                          // 재귀탐색 옵션 주기 중요.
+            Regex regex = new Regex(textBox4.Text); // 정규식 필터링 부분
             if (DateTime.Compare(st, ed) > 0)
                 MessageBox.Show("End Time is earlier than Start Time\r\nPlease Resetting Time");
             foreach (FileInfo file in files)
             {
-                String fileName = file.Name.Replace(extension, "");
+                String fileName = file.Name.Replace(extension, ""); // 세가지 타임옵션 모두 OR 로 둬서 포괄적으로 탐색
                 if (regex.IsMatch(fileName)&&DateTime.Compare(st,ed)<0&&(DateTime.Compare(st,file.CreationTime)<0||
                     DateTime.Compare(st, file.LastAccessTime)<0|| DateTime.Compare(st, file.LastWriteTime)<0||
                     DateTime.Compare(file.CreationTime,ed)>0|| DateTime.Compare(file.LastAccessTime, ed) > 0||
                     DateTime.Compare(file.LastWriteTime, ed) > 0))
-                {
+                { //시간필터링은 생성시간 , 마지막 접근 , 수정 시간 3가지가 있는데 추후에는 각각 나눠서 필터링 하도록 하면 더 좋을듯.
                   
-                    textBox3.AppendText(file.FullName+Environment.NewLine);
+                    textBox3.AppendText(file.FullName+Environment.NewLine); // Log 기록 남기기.
                 }
             }
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e) // Log Clear.
         {
             textBox3.Clear();
         }
-        private void endtime_ValueChanged(object sender, EventArgs e)
+        private void endtime_ValueChanged(object sender, EventArgs e) //시간 입력받는 메서드
         {
             DateTime ed = endtimepicker.Value;
         }
@@ -130,10 +130,10 @@ namespace Capstone_test_1
                    
                 }
             }
-            SaveLog();
+            SaveLog(); 
             System.Diagnostics.Process.Start(textBox2.Text);
         }
-        public void SaveLog()
+        public void SaveLog() // 로그기록을 .txt 파일로 따로 빼주는 메서드
         {/*
              
             StreamWriter sw;
@@ -146,7 +146,7 @@ namespace Capstone_test_1
             */
             using (StreamWriter sw = new StreamWriter(Path.Combine(textBox2.Text, "Log.txt")))
             {
-                sw.Write("수집시간 : " + DateTime.Now + Environment.NewLine);
+                sw.Write("수집시간 : " + DateTime.Now + Environment.NewLine); // CoC 무결성 보장을 위한 TimeStamp.
                 string texts = textBox3.Text;
                 sw.Write(texts);
             }
@@ -155,7 +155,7 @@ namespace Capstone_test_1
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            textBox3.ScrollBars = ScrollBars.Vertical;
+            textBox3.ScrollBars = ScrollBars.Vertical; //Log 스크롤부여
             // Allow the TAB key to be entered in the TextBox control.
             textBox3.AcceptsReturn = true;
             // Allow the TAB key to be entered in the TextBox control.
